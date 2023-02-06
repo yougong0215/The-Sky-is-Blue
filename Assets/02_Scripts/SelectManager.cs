0a40f928b8acde7c92c27152fe1defcbb143efdc
Hyeok <jun44227@gmail.com>
2023-02-07 01:45:52 +0900

    드래그 클릭 + 움직이기
    시프트 누르고 클릭하면 중복선택됨
    드래그로 한번에 유닛 클릭 가능
    우클릭으로 이동

---
+++ Assets/02_Scripts/SelectManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class SelectManager
+{
+    private static SelectManager _instance;
+    public static SelectManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new SelectManager();
+            }
+
+            return _instance;
+        }
+        private set
+        {
+            _instance = value;
+        }
+    }
+    public HashSet<SelectableUnit> SelectedUnits = new HashSet<SelectableUnit>();
+    public List<SelectableUnit> AvailableUnit = new List<SelectableUnit>();
+
+    private SelectManager() { }
+
+    public void Select(SelectableUnit Unit)
+    {
+        SelectedUnits.Add(Unit);
+        Unit.OnSelected();
+    }
+
+    public void Deselect(SelectableUnit Unit)
+    {
+        Unit.OnDeselected();
+        SelectedUnits.Remove(Unit);
+    }
+
+    public void DeselectAll()
+    {
+        foreach(SelectableUnit unit in SelectedUnits)
+        {
+            unit.OnDeselected();
+        }
+        SelectedUnits.Clear();
+    }
+
+    public bool IsSelected(SelectableUnit unit)
+    {
+        return SelectedUnits.Contains(unit);
+    }
+}
---
+++ Assets/02_Scripts/SelectableUnit.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class SelectableUnit : MonoBehaviour
+{
+    private NavMeshAgent Agent;
+    [SerializeField]
+    private SpriteRenderer SelectionSprite;
+
+    private void Awake()
+    {
+        SelectManager.Instance.AvailableUnit.Add(this);
+        Agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void MoveTo(Vector3 Position)
+    {
+        Agent.SetDestination(Position);
+    }
+
+    public void OnSelected()
+    {
+        SelectionSprite.gameObject.SetActive(true);
+    }
+
+    public void OnDeselected()
+    {
+        SelectionSprite.gameObject.SetActive(false);
+    }
+}
